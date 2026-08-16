1dee06dd600881407e0ff2a0220babce55ab44be
mkiuzzal01 <mkiuzzal07@gmail.com>
2026-08-16 20:57:17 +0600

    change the vercel file

--- dist/app/modules/note/note.constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.noteSearchableFields = void 0;
-exports.noteSearchableFields = ['title'];
--- dist/app/modules/post/post.constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.postSearchableFields = void 0;
-exports.postSearchableFields = ['title', 'content'];
